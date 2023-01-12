925d2b1b6fd3d7433269314a5c23521271404952
shane <contact@shane.gg>
2023-01-12 11:16:52 -0600

    Update for Reality Hack 2023

--- repo-setup/create_repositories.py
+++ repo-setup/create_repositories.py
@@ -5,8 +5,11 @@ import sys
 from git import exc, Remote, Repo
 
 BASE_REPO_NAME = "TEAM-"
-NUMBER_OF_TEAMS = 60
-ORGANIZATION = "Reality-Hack-2022"
+NUMBER_OF_TEAMS = 80
+ORGANIZATION = "Reality-Hack-2023"
+REPO_NAME = "reality-hack-2023-repo"
+
+
 with open("personal_access_token", "r") as f:
     ACCESS_TOKEN = f.read().strip()
 
@@ -22,23 +25,32 @@ def create_remote_repository(team_name):
         data=json.dumps(data),
         headers=headers
     )
-    pass
+    print(f"Created {ORGANIZATION}/{team_name}")
 
 
 def add_repository_remote(repo, team_name):
-    Remote.create(
-        repo, team_name,
-        f"https://{ACCESS_TOKEN}@github.com/{ORGANIZATION}/{team_name}.git"
-    )
+    try:
+        Remote.create(
+            repo, team_name,
+            f"https://{ACCESS_TOKEN}@github.com/{ORGANIZATION}/{team_name}.git"
+        )
+        print(f"Added remote for {repo}/{team_name}")
+    except Exception as error:
+        print(error)
+        print("Trying again...")
+        remove_repository_remote(repo, team_name)
+        add_repository_remote(repo, team_name)
 
 
 def push_repository(repo, team_name):
     remote = repo.remotes[team_name]
     remote.push("main")
+    print(f"Pushed {repo}/{team_name}")
 
 
 def remove_repository_remote(repo, team_name):
     Remote.remove(repo, team_name)
+    print(f"Removed remote for {repo}/{team_name}")
 
 
 def delete_remote_repository(team_name):
@@ -50,10 +62,11 @@ def delete_remote_repository(team_name):
         f"https://api.github.com/repos/{ORGANIZATION}/{team_name}",
         headers=headers
     )
+    print(f"Deleted {ORGANIZATION}/{team_name}")
 
 
 if __name__ == "__main__":
-    repo = Repo("../../reality-hack-2022-repo")
+    repo = Repo(f"../../{REPO_NAME}")
     for n in range(NUMBER_OF_TEAMS):
         team_name = str(n + 1)
         if len(team_name) == 1:
